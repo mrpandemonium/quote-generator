1e5f59f37c7570f59ba41d259bc8be1e43c7e54c
mrpandemonium <wincentsouriya@gmail.com>
2023-05-20 13:42:17 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import "./App.css";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>Quotes</div>;
-  }
+function App() {
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setQuote({
+          text: data.content,
+          author: data.author,
+        });
+      });
+  };
+  return (
+    <div className="App">
+      <div id="quote-box">
+        <p id="text">{quote.text}</p>
+        <p id="author">{quote.author}</p>
+        <button id="new-quote" onClick={getQuote}>
+          New Quote
+        </button>
+        <a
+          href={"http://twitter.com/intent/tweet" + quote.text}
+          id="tweet-quote"
+        >
+          Post to Twitter
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
